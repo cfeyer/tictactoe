45eb9c54f6a60091b3a56083a7c6bcffec170532
Chris Feyerchak <chris@feyerchak.net>
2020-12-19 19:12:27 -0500

    Fix draw game. Implement interface and factory classes.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-class PlayerAgent:
+from abc import ABC, abstractmethod
+
+class PlayerAgentInterface(ABC):
+
+    @abstractmethod
+    def notify_other_players_move(self, r, c):
+        pass
+
+    @abstractmethod
+    def request_move(self):
+        pass
+
+    @abstractmethod
+    def notify_game_over(self, outcome):
+        pass
+
+
+class InteractiveConsolePlayerAgent(PlayerAgentInterface):
 
     def __init__(self, name):
         self.name = name
@@ -14,6 +31,24 @@ class PlayerAgent:
 
     def notify_game_over(self, outcome):
         print(f'{self.name}: I {outcome}!')
+
+
+class PlayerAgentFactoryInterface(ABC):
+
+    @abstractmethod
+    def make_player_agent(self):
+        pass
+
+class InteractiveConsolePlayerAgentFactory(PlayerAgentFactoryInterface):
+
+    def __init__(self):
+        self.next_player_id = 0
+
+    def make_player_agent(self):
+        agent = InteractiveConsolePlayerAgent(f'Player{self.next_player_id}')
+        self.next_player_id += 1
+        return agent
+
 
 class Board:
     def __init__(self):
@@ -62,32 +97,53 @@ class Board:
         else:
             return self.spaces[1][1] if self.spaces[1][1] == self.spaces[0][1+m] and self.spaces[1][1] == self.spaces[2][1-m] and self.spaces[1][1] != ' ' else None
 
-def main():
-    board = Board()
+class Game:
 
-    player_x = PlayerAgent('Alice')
-    player_o = PlayerAgent('Bob')
+    def __init__(self, player_x, player_o):
+        self.player_x = player_x
+        self.player_o = player_o
 
-    mark = 'x'
-    current_player = player_x
-    other_player = player_o
-
-    board.print()
-    print('')
-
-    while not board.is_full():
-        (r, c) = current_player.request_move()
-        board.set(r, c, mark)
-        other_player.notify_other_players_move(r, c)
+    def play(self):
+        mark = 'x'
+        current_player = self.player_x
+        other_player = self.player_o
+   
+        board = Board()
         board.print()
-        winner = board.find_winner()
-        if winner:
-            print(f'{winner} won')
-            current_player.notify_game_over('win')
-            other_player.notify_game_over('lose')
-            break
+        print('')
 
-        (current_player, other_player) = (other_player, current_player)
-        mark = 'o' if mark == 'x' else 'x'
+        is_game_won = False
+    
+        while not board.is_full():
+            (r, c) = current_player.request_move()
+            board.set(r, c, mark)
+            other_player.notify_other_players_move(r, c)
+            board.print()
+            winner = board.find_winner()
+            if winner:
+                print(f'{winner} won')
+                current_player.notify_game_over('win')
+                other_player.notify_game_over('lose')
+                is_game_won = True
+                break
+    
+            (current_player, other_player) = (other_player, current_player)
+            mark = 'o' if mark == 'x' else 'x'
+
+        if not is_game_won:
+            current_player.notify_game_over('draw')
+            other_player.notify_game_over('draw')
+
+
+def main():
+
+    player_factory = InteractiveConsolePlayerAgentFactory()
+    
+    player_1 = player_factory.make_player_agent()
+    player_2 = player_factory.make_player_agent()
+
+    game = Game(player_1, player_2)
+    game.play()
+
 
 main()
